cfb3807f72ac3c855a2da79ae01c40a2acc0944b
Hizkia Felix Winata <felix@hizkifw.me>
2021-09-16 22:50:14 +0800

    queue

--- bop.py
+++ bop.py
@@ -9,6 +9,7 @@ guild_ids = [730136766748819609]
 players: dict[int, PlayerInstance] = {}
 
 ERR_NOT_IN_VC = 'You must be in a voice channel to use this command.'
+ERR_UNKNOWN = 'An unknown error has occurred.'
 
 @client.event
 async def on_ready():
@@ -31,6 +32,20 @@ def get_player(ctx: SlashContext):
     if vc_id in players:
         return players[vc_id]
     return None
+
+async def get_player_or_connect(ctx: SlashContext, *, reply=False):
+    player = get_player(ctx)
+    if player is None:
+        if not await connect_vc(ctx):
+            if reply:
+                await ctx.send(content=ERR_NOT_IN_VC)
+            return None
+        player = get_player(ctx)
+
+    if player is None and reply:
+        await ctx.send(content=ERR_UNKNOWN)
+
+    return player
 
 @slash.slash(
     name='join',
@@ -74,19 +89,70 @@ async def leave(ctx: SlashContext):
 )
 async def play(ctx: SlashContext, *, url):
     await ctx.defer()
-
-    player = get_player(ctx)
+    player = await get_player_or_connect(ctx, reply=True)
     if player is None:
-        if not await connect_vc(ctx):
-            return await ctx.send(content=ERR_NOT_IN_VC)
-        player = get_player(ctx)
-
-    if player is None:
-        return await ctx.send(content='Error')
+        return
 
     n_queued = await player.queue_url(url)
-    await player.play()
+
+    if not player.is_playing():
+        await player.play()
+
     return await ctx.send(content='{} songs queued'.format(n_queued))
+
+@slash.slash(
+    name='queue',
+    description='Song queue',
+    options=[
+
+    ],
+    guild_ids=guild_ids
+)
+async def queue(ctx: SlashContext):
+    player = get_player(ctx)
+    if player is None:
+        return ctx.send(content='Nothing is playing')
+
+    await ctx.defer()
+    current_index = player.playlist.get_index()
+    slice_start = max(0, current_index - 1)
+    slice_end = current_index + 5
+    partial_list = player.playlist.get_list()[slice_start:slice_end]
+    partial_index = current_index - slice_start
+
+    message = ''
+    for idx, song in enumerate(partial_list):
+        display_idx = slice_start + idx + 1
+        title = await song.get_title()
+        url = song.url
+        duration = await song.get_duration()
+
+        if idx == partial_index:
+            message += '__Now Playing:__\n'
+
+        message += f'{display_idx}. [{title}]({url}) | {duration}\n'
+
+    embed = discord.Embed(
+        title='Queue',
+        description=message,
+    )
+    await ctx.send(embed=embed)
+
+@slash.slash(
+    name='skip',
+    description='Skip current song',
+    guild_ids=guild_ids
+)
+async def skip(ctx: SlashContext):
+    await ctx.defer()
+    player = await get_player_or_connect(ctx, reply=True)
+    if player is None:
+        return
+
+    if await player.play_next():
+        await ctx.send(content='Skipped')
+    else:
+        await ctx.send(content='End of queue')
 
 print('Starting bot')
 client.run(os.environ.get('BOT_TOKEN'))
--- music.py
+++ music.py
@@ -7,7 +7,7 @@ async def extract_info(url):
     def _extract(_url):
         opts = {
             'extract_flat': True,
-            'skip_download': True
+            'skip_download': True,
         }
         with YoutubeDL(opts) as ydl:
             return ydl.extract_info(_url, download=False, process=False)
@@ -38,6 +38,10 @@ class Song():
                 return fmt['url']
         return None
 
+    async def get_duration(self):
+        await self.get_info()
+        return self.info['duration']
+
 class Playlist():
     song_list = []
     current_index = 0
@@ -46,22 +50,43 @@ class Playlist():
         self.song_list.append(song)
 
     def now_playing(self):
-        if self.current_index > len(self.song_list):
+        if self.current_index >= len(self.song_list) or self.current_index < 0:
             return None
         return self.song_list[self.current_index]
 
-    def get_next(self):
-        self.current_index += 1
+    def get_list(self):
+        return self.song_list
+
+    def get_index(self):
+        return self.current_index
+
+    def has_next(self):
+        return self.current_index + 1 < len(self.song_list)
+
+    def has_prev(self):
+        return self.current_index - 1 > 0
+
+    def go_next(self):
+        if self.has_next():
+            self.current_index += 1
+        return self.now_playing()
+
+    def go_prev(self):
+        if self.has_prev():
+            self.current_index -= 1
         return self.now_playing()
 
 class PlayerInstance():
     def __init__(self, voice_client: discord.VoiceClient):
         self.voice_client = voice_client
         self.playlist = Playlist()
+        self.is_skipping = False
 
     async def queue_url(self, url):
         # Check type of URL
         if 'youtu.be' in url or '/watch?v=' in url:
+            # Force youtube-dl to extract video instead of playlist
+            url = url.replace('&list=', '&_list=')
             self.playlist.queue(Song(url))
             return 1
         elif 'youtube.com/playlist' in url:
@@ -76,8 +101,16 @@ class PlayerInstance():
 
         return 0
 
+    def is_playing(self):
+        return self.voice_client.is_playing()
+
     async def play_next(self):
-        self.playlist.get_next()
+        if not self.playlist.has_next():
+            return False
+        self.playlist.go_next()
+        self.is_skipping = True
+        await self.play()
+        return True
 
     async def play(self):
         song = self.playlist.now_playing()
@@ -85,7 +118,21 @@ class PlayerInstance():
             return False
         url = await song.get_audio_url()
         source = await discord.FFmpegOpusAudio.from_probe(url)
-        self.voice_client.play(source)
+
+        if self.voice_client.is_playing():
+            self.voice_client.stop()
+
+        loop = asyncio.get_running_loop()
+        def handle_next(error):
+            if self.is_skipping:
+                self.is_skipping = False
+                return
+            asyncio.run_coroutine_threadsafe(self.play_next(), loop)
+        self.voice_client.play(source, after=handle_next)
 
     async def pause(self):
-        pass
+        self.voice_client.pause()
+
+    async def resume(self):
+        self.voice_client.resume()
+
